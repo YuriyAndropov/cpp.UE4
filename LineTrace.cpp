2b254314f4c176184113161e3b569543fcfab9c4
Yurii Andropov <yuriy.andropov@live.com>
2018-11-02 10:27:46 -0400

    Add files via upload
    line trace snippets

---
+++ LineTrace.cpp
@@ -0,0 +1,46 @@
+//line trace single channel and return bool for hit
+bool UGridManager::TraceHitCheck(FVector StartPoint, FVector EndPoint)
+{
+
+	FHitResult Hit;
+	return GetWorld()->LineTraceSingleByChannel(
+		OUT Hit,
+		StartPoint,
+		EndPoint,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		FCollisionQueryParams::DefaultQueryParam,
+		FCollisionResponseParams::DefaultResponseParam);
+}
+
+//line trace single channel and return hit location
+FVector UGridManager::HitLocation(FVector StartPoint, FVector EndPoint)
+{
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByChannel(
+		OUT Hit,
+		StartPoint,
+		EndPoint,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		FCollisionQueryParams::DefaultQueryParam,
+		FCollisionResponseParams::DefaultResponseParam);
+	return Hit.Location;
+}
+
+//line trace single channel and return name of phys material
+FString UGridManager::HitPhysicMaterial(FVector StartPoint, FVector EndPoint)
+{
+	FCollisionQueryParams QueryParams;
+	QueryParams.bTraceComplex = true;
+	QueryParams.bReturnPhysicalMaterial = true;
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByChannel(
+		OUT Hit,
+		StartPoint,
+		EndPoint,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		QueryParams,
+		FCollisionResponseParams::DefaultResponseParam);
+	UPhysicalMaterial* PhysicsMtl = Hit.PhysMaterial.Get();
+
+	return PhysicsMtl->GetName();
+}
